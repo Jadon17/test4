0db42f0092e922d855418feecc621918275afe60
Jadon17 <74726354+Jadon17@users.noreply.github.com>
2022-04-21 19:52:02 +0530

    Add files via upload

---
+++ flower.js
@@ -0,0 +1,30 @@
+status1 = "";
+objects = [];
+
+function preload(){
+loadImage = ("cars.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center()
+    objectDetecter = ml5.objectDetector('cocossd',modelloaded);
+    document.getElementById("status").innerHTML = "Model Is Detecting the Objects";
+}
+
+function modelloaded(){
+    console.log("MODEL HAS SUCCESSFULLY LOADED !!! ");
+    status1 = true;
+    
+}
+
+function gotresults(error,results){
+    if(error){
+        console.log(error);
+    }
+
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
